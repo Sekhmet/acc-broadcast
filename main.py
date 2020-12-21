17e5e60aa63f814482e1e3535a5027a57c6a7331
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2020-12-21 17:56:53 +0100

    feat: update server URL

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ from connection.websockets import WebSocketsClient
 if __name__ == "__main__":
     gameInfo = ACCGameInfo()
 
-    server = WebSocketsClient("wss://echo.websocket.org", gameInfo)
+    server = WebSocketsClient("wss://accs.sekhmet.pl", gameInfo)
     server.run()
